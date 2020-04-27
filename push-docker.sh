b7417ffb3ad6efe59f29acc3147bbc8de57b2a2d
anushirahatti <aniruddhashirahatti@yahoo.com>
2020-04-26 20:55:52 -0400

    adding locust.io

--- push-docker.sh
+++ push-docker.sh
@@ -10,7 +10,7 @@ dockerpath="anushirahatti/housing-price-prediction-app"
 # Authenticate & Tag
 echo "Docker ID and Image: $dockerpath"
 docker login &&\
-    docker image tag 02e27200c985 $dockerpath
+    docker image tag ade5b630138f $dockerpath
 
 # Push Image
 docker image push $dockerpath 
